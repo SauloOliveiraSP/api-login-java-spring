91144e50eb93b935e8a363e02ed4761d05062e08
Saulo de Oliveira <Sauloliveira.SP@gmail.com>
2024-04-06 19:21:47 -0300

    Inicializando projeto

---
+++ src/main/java/api/login/Application.java
@@ -0,0 +1,25 @@
+package api.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Projeto Spring Boot gerado via Spring Initializr.
+ * - Spring Web
+ * - Spring Data JPA
+ * - H2 DataBase
+ * - OpenFeign
+ *
+ * @author sauol
+ */
+
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/dio/padroesprojetojavaspring/PadroesProjetoJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.padroesprojetojavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjetoJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
